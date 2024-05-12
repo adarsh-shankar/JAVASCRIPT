263b0781fe4f711bd613efae3fd578f99f1f962e
adarsh-shankar <64947207+adarsh-shankar@users.noreply.github.com>
2024-05-12 18:32:05 +0000

    learned about datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+// learning declaration
+const accountID=144553 
+let accountEmail="adarshtripathi11@gmail.com" //Always use this
+var accountPassword="12345"
+/* Prefer not to use var because  of 
+issues in block and functional scope
+*/
+accountCity="jaipur"
+let accountState
+//var , let , const memory me space lega
+//const: value wont be changed : we keep those values here which we dont want to get changed 
+// accountID=2 //wrong line
+// console.log(accountID); // it will show an error
+accountEmail="hc@hc.com"
+accountPassword="21212111"
+accountCity="kasia"
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version , it will be applied to the whole current page 
+
+// alert("Hello")
+// alert(3+3)     we are using node.js not browser, it will work only in browswr   
+console.log(3+3);
+console.log("Adarsh") //code readability is high
+
+//data types in JS
+let name="Hitesh"
+let age=18
+let isLoggedIn=false
+//number=> 2 to power 53 , bigint for bigger value 
+//bigint
+//string=>""
+//boolean => true/false
+//null is a standalone value which means empty but not undefined, we can assign it to any variables
+//undefined is also a value when nothing is defined but initialised
+//symbol =>unique 
+//object
+console.log(typeof "age");
+console.log(typeof null);  // an interview question  NULL IS AN OBJECT , ACCORDING TO FEW PEOPLE , IT MAY BE AN ERROR OF THE JS
+console.log(typeof undefined); //undefined type 
+//typeof can take variables or value directly.
