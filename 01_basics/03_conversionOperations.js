a348ae5cd25811aa93b2673486a87fd11e0968d5
adarsh-shankar <64947207+adarsh-shankar@users.noreply.github.com>
2024-05-12 19:31:38 +0000

    few more conversions and operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,24 +1,58 @@
-let score="33abc"
-console.log(typeof score)
-console.log(typeof(score));
+// let score="33abc"
+// //console.log(typeof score)
+// //console.log(typeof(score));
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber); // an Nan (not a number) 
+// let valueInNumber=Number(score)
+// //console.log(typeof valueInNumber)
+// //console.log(valueInNumber); // an Nan (not a number) 
 
-//"33" => 33
-//"33abc" => Nan
+// //"33" => 33
+// //"33abc" => Nan
 
-let isLoggedIn=true;
+// let isLoggedIn=true;
 
-let booleanIsLoggedIn=Boolean(IsLoggedIn)
-let tocheckstring=""
-let tocheck=Boolean(tocheckstring);
-console.log(tocheck);  //it will show false
-//true<=> 1; false<=>0
-//empty string =>false in boolean
+// let booleanIsLoggedIn=Boolean(IsLoggedIn)
+// let tocheckstring=""
+// let tocheck=Boolean(tocheckstring);
+// //console.log(tocheck);  //it will show false
+// //true<=> 1; false<=>0
+// //empty string =>false in boolean
 
-let someNumber=33
-let stringNumber=String(someNumber)  //here 33 get converted to a string
-console.log(stringNumber); // it will show 33 as output 
-console.log(typeof stringNumber);  // ot will show as string
+// let someNumber=33
+// let stringNumber=String(someNumber)  //here 33 get converted to a string
+// //console.log(stringNumber); // it will show 33 as output 
+// //console.log(typeof stringNumber);  // ot will show as string
+
+//**********************operations*****************************//
+let value=3
+let negValue= -value
+//console.log(negValue);
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2); //4
+// console.log(2**3); //8
+// console.log(2/2); //1
+// console.log(2%3); //2 (think here)
+let str1="hello"
+let str2="adarsh"
+let str3= str1+str2;
+// console.log(str3); //hello adarsh
+// console.log("1"+2); // 12
+// console.log(1+"2");  // 12
+// console.log("1"+2+2); //14 was what i thinking 
+// console.log(1+2+"2");// 32 is correct here
+// console.log((3+4)*5%3); // be readble always
+
+console.log(true); // output is true
+console.log(+true); //output is 1
+console.log(+""); //output is 1
+let num1,num2,num3
+num1=num2=num3=2+2 
+
+let gameCounter=100
+gameCounter++; //it will set gameCounter to 101 in next line
+console.log(gameCounter); //  here it will 101
+++gameCounter;
+console.log(gameCounter); 
+
+
