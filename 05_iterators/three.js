6a6466fb3aab9774441ac03ea54c2dc24973da36
adarsh-shankar <64947207+adarsh-shankar@users.noreply.github.com>
2024-06-04 14:52:00 +0000

    revised

--- 05_iterators/three.js
+++ 05_iterators/three.js
@@ -6,7 +6,7 @@
 const arr = [1, 2, 3, 4, 5]
 
 // for (const num of object) {  //kis object pe loop lgana chaahte hai , not object{} actually
-//                              //for example array,string,map,object
+//                              //for example array,string,map
 // }
 
 for (const num of arr) {
@@ -18,7 +18,34 @@ for (const greet of greetings) {     //here i fetched the single character of st
     console.log(`Each char is ${greet}`)
 }
 
-// Map is an object holds key value pair 
+// The Map object holds key-value pairs and remembers the original insertion order of the keys. 
+//Any value (both objects and primitive values) may be used as either a key or a value.
+
+/*
+
+    const map1 = new Map();
+
+    map1.set('a', 1);
+    map1.set('b', 2);
+    map1.set('c', 3);
+
+    console.log(map1.get('a'));
+    // Expected output: 1
+
+    map1.set('a', 97);
+
+    console.log(map1.get('a'));
+    // Expected output: 97
+
+    console.log(map1.size);
+    // Expected output: 3
+
+    map1.delete('b');
+
+    console.log(map1.size);
+    // Expected output: 2
+
+*/
 
 const map = new Map()
 map.set('IN', "India")
